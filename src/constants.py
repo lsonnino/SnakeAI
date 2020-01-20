ac698756556eef646bed2c3c63aedd43127374eb
lsonnino <lorenzo@sonnino.com>
2020-01-20 12:22:03 +0100

    Trained AI

--- src/constants.py
+++ src/constants.py
@@ -12,8 +12,8 @@
 NAME = "SnakeAI"
 AI_PLAYS = True
 NUMBER_OF_GAMES = 1000  # -1 to play infinite games
-SAVE_EVERY = 50  # Must be greater than 0
-LOAD_NUMBER = 150  # -1 to start from the beginning
+SAVE_EVERY = 100  # Must be greater than 0
+LOAD_NUMBER = 200  # -1 to start from the beginning
 DATA_DIR = "data"
 EXTENSION = "snake"
 PARAMS_EXTENSION = "params"
